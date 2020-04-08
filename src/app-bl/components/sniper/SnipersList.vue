b7370042b1c71e4cb1446a8256669dded1cedc62
marcelloburattini79 <burattinimarcello@yahoo.it>
2020-04-08 10:59:51 +0200

    mapGetters

--- src/app-bl/components/sniper/SnipersList.vue
+++ src/app-bl/components/sniper/SnipersList.vue
@@ -4,17 +4,21 @@
 
 
 <script>
+import {mapGetters} from 'vuex';
+
 export default {
     name: "SnipersList",
+
+    computed: {
+        ...mapGetters([
+            'sniperItems'
+        ])
+    },
 
     created(){
         this.$store.dispatch('getSniperItems');
     },
-    computed(){
-        sniperItems(){
-            return this.$store.getters.sniperItems;
-        }
-    }
+    
     
 }
 </script>
