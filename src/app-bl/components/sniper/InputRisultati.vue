b33b9f31463abb873891e9b5d339ff6f7887a113
GTA-V <marcellojes@gmail.com>
2020-04-15 00:50:03 +0200

    controllo campi vuoti ok

--- src/app-bl/components/sniper/InputRisultati.vue
+++ src/app-bl/components/sniper/InputRisultati.vue
@@ -5,21 +5,31 @@
             <div class="field">
                 <label>Trofei</label>
                 <input v-model="fields.trofei" type="number" placeholder="Inserisci i trofei guadagnati" />
+                <span style="color: red">{{ fieldErrors.trofei }}</span>
             </div>
 
             <div class="field">
                 <label>Danni</label>
                 <input v-model="fields.danni" type="number" placeholder="Inserisci i danni inflitti" />
+                <span style="color: red">{{ fieldErrors.danni }}</span>
             </div>
 
             <div class="field">
                 <label>Kill</label>
                 <input v-model="fields.kill" type="number" placeholder="Inserisci le kill fatte" />
+                <span style="color: red">{{ fieldErrors.kill }}</span>
             </div>
 
             <div class="field">
                 <label>Posizione</label>
                 <input v-model="fields.posizione" type="number" placeholder="Inserisci la posizione raggiunta" />
+                <span style="color: red">{{ fieldErrors.posizione }}</span>
+            </div>
+
+            <div class="field">
+                <label>Partite</label>
+                <input v-model="fields.partite" type="number" placeholder="Inserisci le partire giocate" />
+                <span style="color: red">{{ fieldErrors.partite }}</span>
             </div>
 
             <div class="field">
@@ -30,8 +40,12 @@
                         {{sniper.nome}}
                     </option>
                 </select>
+                <span style="color: red">{{ fieldErrors.sniper }}</span>
             </div>
 
+            <div class="paddingInf50">
+                <button class="button is-primary" @click="submitForm">Submit</button>
+            </div>
         </form>
 
     </div>
@@ -51,6 +65,7 @@ export default {
                 danni: null,
                 kill: null,
                 posizione: null,
+                partite:null,
                 sniper: null
             },
 
@@ -59,6 +74,7 @@ export default {
                 danni: undefined,
                 kill: undefined,
                 posizione: undefined,
+                partite: undefined,
                 sniper: undefined
             },
         }
@@ -66,7 +82,21 @@ export default {
 
     methods:{
         submitForm(evt) {
+            evt.preventDefault();
+            this.fieldErrors = this.validateForm(this.fields);
+            if (Object.keys(this.fieldErrors).length) {return};
+        },
 
+        validateForm(fields){
+            const errors= {};
+            if (!fields.trofei){errors.trofei = 'Inserire il numero di trofei'};
+            if (!fields.danni){errors.danni = 'Inserire il numero di danni'};
+            if (!fields.kill){errors.kill = 'Inserire il numero di kill'};
+            if (!fields.posizione){errors.posizione = 'Inserire il numero di posizione'};
+            if (!fields.partite){errors.partite = 'Inserire il numero di partite'};
+            if (!fields.sniper){errors.sniper = 'Selezionare uno sniper'};
+
+            return errors;
         }
     }
     
@@ -75,5 +105,7 @@ export default {
 
 
 <style>
-
+.paddingInf50{
+    padding-bottom: 50px;
+}
 </style>
--- src/app-bl/components/sniper/SnipersList.vue
+++ src/app-bl/components/sniper/SnipersList.vue
@@ -1,14 +1,20 @@
 <template>
     <div class="container is-fullhd">
+            
+            <div>
+                <img class="image is-512" src="https://i.ibb.co/w0zFKvn/Battlelands-Screenshot-2020-04-13-19-48-23.jpg">
+            </div>
+            
             <div class = "columns">
                 <div class="column is-half">
+
                     <article class="media noBorder" 
                         v-for="sniper in sniperItemsDanni" 
                         v-bind:key="sniper.id"
                         v-bind:class="{ 'blue-border': sniper.trofei >= 20 }">
                         <SniperSheet
                         v-bind:sniper="sniper"
-                        v-bind:rank="sniperItemsDanni.indexOf(sniper) + 1">
+                        v-bind:rank="sniperItemsDanni.indexOf(sniper)">
                         </SniperSheet>
                     </article>
                 </div>
@@ -20,7 +26,7 @@
                         v-bind:class="{ 'blue-border': sniper.trofei >= 20 }">
                         <SniperSheet
                         v-bind:sniper="sniper"
-                        v-bind:rank="sniperItemsKill.indexOf(sniper) + 1">
+                        v-bind:rank="sniperItemsKill.indexOf(sniper)">
                         </SniperSheet>
                     </article>
                 </div>
@@ -79,7 +85,13 @@
 
 
 <style>
-    .wrapper{
-        width: auto;
-    }
+    .image.is-512{
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+  padding: 20px;
+  padding-bottom: 50px;
+
+}
 </style>
